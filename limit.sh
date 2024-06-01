0f7d28278b98dd06eb43c61456f0dec665184706
izulx1 <166265491+izulx1@users.noreply.github.com>
2024-06-01 18:09:40 +0700

    Update limit.sh

--- limit.sh
+++ limit.sh
@@ -7,7 +7,13 @@ fi
 wget -O /usr/sbin/limit-ip-ssh "https://raw.githubusercontent.com/izulx1/limit/master/limit-ip-ssh"
 chmod +x /usr/sbin/limit-ip-ssh
 
+anu=$(cat /etc/crontab | grep "limit-ip-ssh" | wc -l)
+if [[ $anu == "1" ]]; then
+sleep 2
+else
 echo "*/1 * * * * root /usr/sbin/limit-ip-ssh" > /etc/crontab
+fi
+
 systemctl restart cron
 echo "Done, Installed Limit IP SSH Successfully"
 sleep 2
